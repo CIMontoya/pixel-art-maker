a22b8a9083b2a84e6f19e5a19dccbf28971d1399
CIMontoya <cimontoya31@gmail.com>
2019-03-12 15:10:54 -0600

    saves drawings to local storage

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ document.addEventListener("DOMContentLoaded", function(){
   var colorPicker = document.querySelector(".colorPick")
   var mouseIsDown = false
   var currentColorIndicator = document.querySelector(".currentColor")
+  var saveButton = document.querySelector(".saveBut")
+  var submitButton = document.querySelector(".submit")
+  var loadButton = document.querySelector(".loadBut")
+  var array = []
+  var availableDrawings
 
   function changeSelectedColor(e) {
     currentColor = e.target.classList[1]
@@ -35,15 +40,85 @@ document.addEventListener("DOMContentLoaded", function(){
     }
   }
 
+  function openSaveForm(e) {
+    e.target.parentNode.parentNode.parentNode.childNodes[7].className = "showForm"
+  }
+
+  function saveDrawing(e) {
+    var divs = document.querySelectorAll(".div")
+    for(var k = 0; k < divs.length; k++) {
+      array.push(divs[k].outerHTML.substring(divs[k].outerHTML.indexOf(":") + 2, divs[k].outerHTML.indexOf(";")))
+    }
+    localStorage.setItem(e.target.parentNode.children[1].value, JSON.stringify(array))
+  }
+
+  function removeElement(elementId) {
+    // Removes an element from the document
+    var element = document.getElementById(elementId);
+    element.parentNode.removeChild(element);
+}
+
+function allStorage() {
+
+    var keys = Object.keys(localStorage)
+
+    return keys
+}
+
+  function showAvailableDrawings(e) {
+    let keys = allStorage()
+    if(loadButton.children.length === 1) {
+      keys.map(key => {
+        let option = document.createElement("option")
+        option.value = key
+        option.innerHTML = key
+        loadButton.append(option)
+      })
+    }
+    availableDrawings = document.querySelector('.loadBut')
+    availableDrawings.addEventListener("change", loadDrawing)
+  }
+
+  function loadDrawing(e) {
+      let colors = JSON.parse(localStorage.getItem(e.target.selectedOptions[0].value))
+      var divs = document.querySelectorAll(".div")
+      for(var k = 0; k < divs.length; k++) {
+        removeElement(divs[k].id)
+      }
+
+      for(var i = 0; i < colors.length;i++) {
+        if((i) % 65 === 0 || i === 0) {
+          var pixels = document.createElement("div")
+          pixels.classList.add("div", "borderLeft")
+          pixels.style.background = colors[i]
+        }
+        else {
+          pixels = document.createElement("div")
+          pixels.classList.add("div")
+          pixels.style.background = colors[i]
+        }
+        console.log(colors[i])
+        pixels.id = i
+        pixels.onmousedown = changePixelColor
+        pixels.addEventListener('mousedown', mouseDown)
+        pixels.addEventListener('mouseup', mouseUp)
+        pixels.onmouseenter = changePixelColorByDrag
+        row.append(pixels)
+      }
+  }
+
   for(var i = 1; i <= 1950;i++) {
     if((i - 1) % 65 === 0 || i === 1) {
       var pixels = document.createElement("div")
       pixels.classList.add("div", "borderLeft")
+      pixels.style.background = "white"
     }
     else {
       pixels = document.createElement("div")
       pixels.classList.add("div")
+      pixels.style.background = "white"
     }
+    pixels.id = i
     pixels.onmousedown = changePixelColor
     pixels.addEventListener('mousedown', mouseDown)
     pixels.addEventListener('mouseup', mouseUp)
@@ -54,5 +129,11 @@ document.addEventListener("DOMContentLoaded", function(){
   for(var j = 0; j < colorDivs.length;j++) {
     colorDivs[j].addEventListener("click", changeSelectedColor)
   }
+
   colorPicker.addEventListener("change", userColorChoice)
+
+  saveButton.addEventListener("click", openSaveForm)
+  submitButton.addEventListener("click", saveDrawing)
+
+  loadButton.addEventListener("click", showAvailableDrawings)
 })
